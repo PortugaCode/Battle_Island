0da2351bde4595fa5656373e1b187b9d13fc2f1f
Jinwon Kim <jinwon0707@gmail.com>
2023-10-31 10:14:04 +0900

    [Fix] Fix Player Animation
    이동 애니메이션 부드럽게 수정

--- Assets/3.Script/Jinwon/Player/PlayerInput.cs
+++ Assets/3.Script/Jinwon/Player/PlayerInput.cs
@@ -6,6 +6,7 @@ public class PlayerInput : MonoBehaviour
 {
     private PlayerMove playerMove;
     private bool isRun;
+    private float z = 0;
 
     private void Awake()
     {
@@ -20,11 +21,6 @@ public class PlayerInput : MonoBehaviour
     private void GetInput()
     {
         float x = Input.GetAxis("Horizontal"); // x축 입력 받기
-        float z = Input.GetAxis("Vertical"); // z축 입력 받기
-
-        playerMove.direction = new Vector3(x, 0, z); // x축, z축 입력값을 playerMove의 direction에 할당
-        playerMove.moveSpeedX = x; // x축 입력값 할당
-        
 
         if (Input.GetKey(KeyCode.LeftShift)) // LeftShift 입력 시 달리기
         {
@@ -36,14 +32,56 @@ public class PlayerInput : MonoBehaviour
             isRun = false;
         }
 
-        if (isRun)
+        if (Input.GetKey(KeyCode.W)) // 앞으로
         {
-            playerMove.moveSpeedZ = z * 2; // z축 입력값 할당
+            if (isRun == true)
+            {
+                if (z < 2)
+                {
+                    z += Time.deltaTime * 2.0f;
+                }
+
+                if (z > 2) z = 2;
+            }
+            else if (isRun == false)
+            {
+                if (z < 1)
+                {
+                    z += Time.deltaTime * 2.0f;
+                }
+
+                if (z > 1) z -= Time.deltaTime * 2.0f;
+            }
         }
-        else
+        else if (Input.GetKey(KeyCode.S)) // 뒤로
         {
-            playerMove.moveSpeedZ = z; // z축 입력값 할당
+            if (z > -1)
+            {
+                z -= Time.deltaTime * 2.0f;
+            }
+
+            if (z < -1) z = -1;
         }
-        
+        else // z축 입력 없음
+        {
+            if (z > 0)
+            {
+                z -= Time.deltaTime * 10.0f;
+            }
+            else if (z < 0)
+            {
+                z += Time.deltaTime * 10.0f;
+            }
+
+            if (Mathf.Abs(z) <= 0.001f)
+            {
+                z = 0;
+            }
+        }
+
+        playerMove.direction = new Vector3(x, 0, z); // x축, z축 입력값을 playerMove의 direction에 할당
+        playerMove.moveSpeedX = x; // x축 입력값 할당
+        playerMove.moveSpeedZ = z; // z축 입력값 할당
+
     }
 }
