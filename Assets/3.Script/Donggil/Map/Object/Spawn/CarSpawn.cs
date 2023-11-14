835bbb504c8128072a5cc8bd799bb24599bff154
ssbaring <lldgll@naver.com>
2023-11-14 10:26:18 +0900

    [Feat] Barricate, Rubbish, Car
    도로에 바리케이트, 쓰레기 추가
    탱크는 이제 자기자신이 겹치면 사라짐
    랜덤오브젝트스폰 스크립트에 조건(isRoad) 추가

--- Assets/3.Script/Donggil/Map/Object/Spawn/CarSpawn.cs
+++ Assets/3.Script/Donggil/Map/Object/Spawn/CarSpawn.cs
@@ -6,27 +6,39 @@ public class CarSpawn : MonoBehaviour
 {
     public GameObject car;
     public GameObject destroyedCar;
-
+    public GameObject barricate;
 
     private void Start()
     {
+       
         Spawn();
     }
 
 
+
     private void Spawn()
     {
+
         int percentage = Random.Range(0, 100);
 
         Vector3 randomAngle = new Vector3(0, Random.Range(0, 360), 0);
 
         if (percentage > 92)
         {
-            Instantiate(car, transform.position + new Vector3(0, 3, 0), Quaternion.Euler(randomAngle), transform);
+            Instantiate(car, transform.position + new Vector3(0, 2, 0), Quaternion.Euler(randomAngle), transform);
+
         }
-        else if(percentage < 20)
+        else if (percentage < 20)
         {
-            Instantiate(destroyedCar, transform.position + new Vector3(0, 3, 0), Quaternion.Euler(randomAngle), transform);
+            Instantiate(destroyedCar, transform.position + new Vector3(0, 2, 0), Quaternion.Euler(randomAngle), transform);
+
+        }
+        else if (percentage >= 20 && percentage < 30)
+        {
+            for (int i = 0; i < Random.Range(1, 3); i++)
+            {
+                Instantiate(barricate, transform.position + new Vector3(Random.Range(-1, 1) * 3, 0, 0), Quaternion.identity, transform);
+            }
         }
     }
 }
