671482a502f0f56bddbcaa71b7be48a1f58edd51
AurochsJay <poolutoocaa123@gmail.com>
2023-11-15 13:20:32 +0900

    [Feat] MainGameUI

---
+++ Assets/3.Script/Junggeun/ETC/GameUIControll.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIControll : MonoBehaviour
+{
+    [Header("Çï¸ä, ¹æ¾î±¸, °¡¹æ")]
+    [SerializeField] private Image helmatImg;
+    [SerializeField] private Image armorImg;
+    [SerializeField] private Image bagImg;
+    [SerializeField] private Image rifleImg1;
+
+    public bool isHelmet = false;
+    public bool isArmor = false;
+    public bool isBag = false;
+    public bool isRifle1 = false;
+
+
+
+    [Header("1¹øÀº ¹«±â, 2¹øÀº ¼ö·ùÅº")]
+    [SerializeField] private Image rifleImg2;
+    [SerializeField] private Image throwImg;
+
+    public bool isRifle2 = false;
+    public bool isThrow = false;
+
+
+    [Header("ÃÑ¾Ë ³²Àº °Í")]
+    public Text ammoText;
+
+    [Header("¼ö·ùÅº °¹¼ö")]
+    public Text throwText;
+
+    [Header("HP ¹Ù")]
+    public Slider hpbar;
+
+    private void Update()
+    {
+        helmatImg.enabled = isHelmet;
+        armorImg.enabled = isArmor;
+        bagImg.enabled = isBag;
+        rifleImg1.enabled = isRifle1;
+
+        rifleImg2.enabled = isRifle2;
+        throwImg.enabled = isThrow;
+    }
+
+}
