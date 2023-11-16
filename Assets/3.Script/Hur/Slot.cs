b4b4ce401a7e3d2eb8601f61551c563b07861f80
HurDoyun <0506hdy@naver.com>
2023-11-16 13:17:09 +0900

    Revert "[feat] pull ready2"
    This reverts commit 023963e0c3be396a635c2b1f76b2efbe9c2698f8.

--- Assets/3.Script/Hur/Slot.cs
+++ Assets/3.Script/Hur/Slot.cs
@@ -17,47 +17,11 @@ public class Slot : MonoBehaviour, IPointerClickHandler
     }
     public void OnPointerClick(PointerEventData eventData)
     {
-<<<<<<< Updated upstream
         Debug.Log($"{id}");
         IvnCont.matchingID = id;
         IvnCont.CreateItem();
         slotManager.ItemID.Remove(id);
         Destroy(gameObject);
-=======
-        if (player.getItem)
-        {
-            player.getItem = false;
-            MeetItem();
-        }
-
-        GiveData(_itemID);
-    }
-
-    private void MeetItem() //아이템 감지하면 슬롯개수 ++
-    {
-        count++;
-        PrefabPlus(count);
-    }
-
-    private void PrefabPlus(int count)
-    {
-        GameObject newSlot = Instantiate(slotPrefab);
-        newSlot.transform.position = slotHolder.position + new Vector3(count * 0f, 0f, 0f);
-    }
-
-    private void GiveData(int _itemID) //수정
-    {
-        ItemData_hur newItem = database.GetItemByID(_itemID);
-
-        if (newItem != null)
-        {
-            //newItem.itemID = _itemID; 
-        }
-        else
-        {
-            //Debug.Log("해당 ID값을 가진 아이템은 없습니다");
-        }
->>>>>>> Stashed changes
     }
 }
 
