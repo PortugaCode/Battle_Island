ab116a2c823c44cdabb5f0675c96ac503754e542
ssbaring <lldgll@naver.com>
2023-11-23 01:23:09 +0900

    [Fix] Resolution Option
    해상도 옵션 수정

--- Assets/3.Script/Donggil/ETC/ResolutionManager.cs
+++ Assets/3.Script/Donggil/ETC/ResolutionManager.cs
@@ -7,9 +7,12 @@ public class ResolutionManager : MonoBehaviour
 {
     FullScreenMode screenMode;
     public Dropdown resolutionDrop;
+    public Dropdown frameListDrop;
     public Toggle fullscreenButton;
-    List<Resolution> resolutions = new List<Resolution>();
-    int resolutionNum;
+    List<(int, int)> resolutionList = new List<(int, int)>() { (960, 540), (1280, 720), (1600, 900), (1920, 1080), (2560, 1440), (3840, 2160) };
+    List<int> frameList = new List<int>() { 30, 60, 120, 144, 165, 240, 0 };
+    public int resolutionNum;
+    public int frameNum;
 
     public GameObject option;
 
@@ -20,30 +23,41 @@ public class ResolutionManager : MonoBehaviour
 
     private void Init()
     {
-        for (int i = 0; i < Screen.resolutions.Length; i++)
-        {
-            /*if (Screen.resolutions[i].refreshRate == 60)
-            {
-                resolutions.Add(Screen.resolutions[i]);
-            }*/
-            resolutions.Add(Screen.resolutions[i]);
-        }
+        Debug.Log(Screen.currentResolution);
         resolutionDrop.options.Clear();
+        frameListDrop.options.Clear();
 
         int optionNum = 0;
-        foreach (Resolution item in resolutions)
+        for (int i = 0; i < resolutionList.Count; i++)
         {
             Dropdown.OptionData option = new Dropdown.OptionData();
-            option.text = item.width + " x " + item.height + "    " + item.refreshRate + "hz";
+            option.text = resolutionList[i].Item1.ToString() + " x " + resolutionList[i].Item2.ToString();
             resolutionDrop.options.Add(option);
 
-            if (item.width == Screen.width && item.height == Screen.height)
+            if (resolutionList[i].Item1 == Screen.width && resolutionList[i].Item2 == Screen.height)
             {
                 resolutionDrop.value = optionNum;
             }
+
             optionNum++;
         }
         resolutionDrop.RefreshShownValue();
+
+        int frameOptionNum = 0;
+        for (int i = 0; i < resolutionList.Count; i++)
+        {
+            Dropdown.OptionData frameOption = new Dropdown.OptionData();
+            frameOption.text = frameList[i].ToString() + "hz";
+            frameListDrop.options.Add(frameOption);
+
+            if (frameList[i] == Screen.currentResolution.refreshRate)
+            {
+                frameListDrop.value = frameOptionNum;
+            }
+
+            frameOptionNum++;
+        }
+        frameListDrop.RefreshShownValue();
 
         fullscreenButton.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
     }
@@ -53,6 +67,11 @@ public class ResolutionManager : MonoBehaviour
         resolutionNum = x;
     }
 
+    public void DropboxFrameChange(int x)
+    {
+        frameNum = x;
+    }
+
     public void Fullscreen(bool isFull)
     {
         screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
@@ -60,7 +79,7 @@ public class ResolutionManager : MonoBehaviour
 
     public void ApplyButton()
     {
-        Debug.Log("적용");
-        Screen.SetResolution(resolutions[resolutionNum].width, resolutions[resolutionNum].height, screenMode);
+        Screen.SetResolution(resolutionList[resolutionNum].Item1, resolutionList[resolutionNum].Item2, screenMode, frameList[frameNum]);
+        Debug.Log("현재 화면 상태" + Screen.currentResolution);
     }
 }
